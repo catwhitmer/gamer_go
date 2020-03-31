4ea85ad6b12621d13eb8926579651a83eb5ffb3d
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-31 19:24:24 +0000

    fixed the get and post users routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,10 @@ class SessionsController < ApplicationController
     
     if  @user.authenticate(params[:password])
       session[:user_id] = @user.id 
-      redirect "users/#{@user.id}"
+      redirect "/users/#{@user.id}"
     else
       redirect '/login'
     end
-  end
-  
-  get '/users/:id' do 
-    @user = User.find_by_id(params[:id])
-    erb :show
   end
   
   get '/signup' do 
@@ -25,8 +20,15 @@ class SessionsController < ApplicationController
   end
   
   post '/users' do 
-    @user = User.find_by(email: params[:email], password_digest: params[:password])
-    erb :show
+   @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+   @user.save
+   session[:user_id] = @user.id
+   redirect "/users/#{@user.id}"
+  end
+  
+  get '/users/:id' do 
+    @user = User.find_by_id(params[:id])
+    redirect "/users/#{@user.id}"
   end
   
 end
