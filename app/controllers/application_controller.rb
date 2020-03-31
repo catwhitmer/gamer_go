680bac29d1833d1a317141e65109f133b69219e4
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-31 12:55:24 +0000

    set up sessions in application contoller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sesions
+    set :sesion_secret, "secret"
   end
 
   get "/" do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
   end
   
   post '/login' do 
+    @user = User.find_by(email: params[:email], password_digest: params[:password])
     
+    if user && @user.authenticate(params[:password])
+      login[:user_id] = user.id 
+      
+    else
+      redirect '/login'
+    end
   end
   
 end
