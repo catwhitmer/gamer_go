cb40ac2068490f64d970a8c0db2737361d4a3294
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-31 23:50:55 +0000

    added post games route with if else statements

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,13 @@ class GamesController < ApplicationController
   end
   
   post '/games' do 
-    
+    if logged_in? && params[:name] != ""
+      @games = Game.new(name: params[:name], genre: params[:genre)
+      @games.save
+      redirect "/games/#{@games.id}"
+    else
+      redirect '/'
+    end
   end
 end
   
