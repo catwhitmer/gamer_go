58cc2a86f168d5e255be4ddbb52a1f2f73ed3242
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-05 13:13:16 +0000

    added more detail to the welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,17 @@ class SessionsController < ApplicationController
     erb :login
   end
   
+  post '/login' do 
+    @user = User.find_by(email: params[:email])
+    
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/login'
+    end
+  end
+  
   get '/users/:id' do 
     @user = User.find_by_id(params[:id])
     erb :"/users/home"
@@ -21,17 +32,6 @@ class SessionsController < ApplicationController
       redirect to "/users/#{@user.id}"
     else
       redirect to '/signup'
-    end
-  end
-  
-  post '/login' do 
-    @user = User.find_by(email: params[:email])
-    
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id 
-      redirect "/users/#{@user.id}"
-    else
-      redirect '/login'
     end
   end
   
