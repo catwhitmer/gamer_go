bb8b5cabe813671fffcb07994ac18a76b2434769
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-05 12:53:30 +0000

    cleaned up code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,42 +15,34 @@ class GamesController < ApplicationController
   end
   
   post '/games' do 
-    if logged_in?
-      @game = Game.create(name: params[:name], genre: params[:genre], notes: params[:notes], user_id: current_user.id)
-      redirect "/games/#{@game.id}"
-    else
-      redirect :'/games/new'
-    end
+    @game = Game.create(name: params[:name], genre: params[:genre], notes: params[:notes])
+    redirect "/games/#{@game.id}"
   end
   
   get '/games/:id/edit' do
-    if logged_in?
-      @game = Game.find_by_id(params[:id])
-      erb :'/games/edit'
-    else 
-      redirect :'/login'
-    end
+    @game = Game.find_by_id(params[:id])
+      if @game
+        erb :'/games/edit'
+      else 
+        redirect :'/login'
+      end
   end
   
   patch '/games/:id' do
-    if logged_in?
       @game = Game.find_by_id(params[:id])
       @game.name = params[:name]
       @game.genre = params[:genre]
       @game.notes = params[:notes]
       @game.save
       redirect "/games/#{@game.id}"
-    else 
-      redirect :'/'
-    end
   end
   
   delete '/games/:id' do
-    if current_user
-      Game.find_by_id(params[:id]).destroy
-      redirect '/games'
-    else 
-      redirect '/games'
-    end
+    @game = Game.find_by_id(params[:id])
+      if @game.destroy
+        redirect '/games'
+      else 
+        redirect '/games'
+      end
   end
 end
